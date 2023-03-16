eb764b56408ff6a52c20a560a20664193385225e
tp47 <adadu@tutanota.com>
2023-03-16 23:20:12 +0400

    Add create user dto

---
+++ src/user/dtos/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from "class-validator";
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsAlphanumeric()
+  @MinLength(3)
+  @IsNotEmpty()
+  userName: string;
+}
