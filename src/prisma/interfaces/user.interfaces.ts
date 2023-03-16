bbcc63765ac435712428ad506325ebb2a5f8ba0a
tp47 <adadu@tutanota.com>
2023-03-16 22:47:56 +0400

    Add UserGetAllParams interface

--- src/prisma/interfaces/user.interfaces.ts
+++ src/prisma/interfaces/user.interfaces.ts
@@ -1,3 +1,11 @@
-import { User } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 
 export type UserWithoutHash = Omit<User, "hash">;
+
+export interface UserGetAllParams {
+  skip?: number;
+  take?: number;
+  cursor?: Prisma.UserWhereUniqueInput;
+  where?: Prisma.UserWhereInput;
+  orderBy?: Prisma.UserOrderByWithRelationInput;
+}
