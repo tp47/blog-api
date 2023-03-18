63fef27eee63008e8ea532b3513152573b2a1e0e
tp47 <adadu@tutanota.com>
2023-03-19 00:24:31 +0400

    Add UserService getUniqueUser method integration tests

--- test/integration/user/user.service.int-spec.ts
+++ test/integration/user/user.service.int-spec.ts
@@ -1,12 +1,23 @@
 import { Test } from "@nestjs/testing";
 import { Prisma } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
+import {
+  UserExistsException,
+  UserNotFoundException,
+} from "src/user/exceptions";
 import { UserModule } from "src/user/user.module";
 import { UserService } from "src/user/user.service";
 
 describe("UserService integration tests", () => {
   let prisma: PrismaService;
   let userService: UserService;
+  let userId: number;
+  const userData: Prisma.UserCreateInput = {
+    email: "johndoe@test.com",
+    userName: "john",
+    hash: "abcdef",
+  };
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [UserModule],
@@ -18,31 +29,25 @@ describe("UserService integration tests", () => {
     await prisma.cleanDatabase();
   });
 
-  describe("createUser", () => {
-    const createUserData: Prisma.UserCreateInput = {
-      email: "johndoe@test.com",
-      userName: "john",
-      hash: "abcdef",
-    };
-
+  describe("createUser method", () => {
     it("should create user", async () => {
-      const user = await userService.createUser(createUserData);
+      const user = await userService.createUser(userData);
+      userId = user.id;
       expect(user).toMatchObject({
-        email: createUserData.email,
-        userName: createUserData.userName,
+        email: userData.email,
+        userName: userData.userName,
       });
     });
 
     it("should throw if email is not unique", async () => {
       try {
         await userService.createUser({
-          email: createUserData.email,
+          email: userData.email,
           userName: "jane",
-          hash: createUserData.hash,
+          hash: userData.hash,
         });
       } catch (error) {
-        expect(error).toBeInstanceOf(Prisma.PrismaClientKnownRequestError);
-        expect(error).toHaveProperty("code", "P2002");
+        expect(error).toBeInstanceOf(UserExistsException);
       }
     });
 
@@ -50,12 +55,72 @@ describe("UserService integration tests", () => {
       try {
         await userService.createUser({
           email: "janedoe@test.com",
-          userName: createUserData.userName,
-          hash: createUserData.hash,
+          userName: userData.userName,
+          hash: userData.hash,
         });
       } catch (error) {
-        expect(error).toBeInstanceOf(Prisma.PrismaClientKnownRequestError);
-        expect(error).toHaveProperty("code", "P2002");
+        expect(error).toBeInstanceOf(UserExistsException);
+      }
+    });
+  });
+
+  describe("getUniqueUser method", () => {
+    it("should get unique user by id", async () => {
+      const user = await userService.getUniqueUser({ id: userId });
+      expect(user).toMatchObject({
+        id: userId,
+        email: userData.email,
+        userName: userData.userName,
+      });
+    });
+
+    it("should get unique user by email", async () => {
+      const user = await userService.getUniqueUser({ email: userData.email });
+      expect(user).toMatchObject({
+        id: userId,
+        email: userData.email,
+        userName: userData.userName,
+      });
+    });
+
+    it("should get unique user by username", async () => {
+      const user = await userService.getUniqueUser({
+        userName: userData.userName,
+      });
+      expect(user).toMatchObject({
+        id: userId,
+        email: userData.email,
+        userName: userData.userName,
+      });
+    });
+
+    it("should throw if id does not exist", async () => {
+      try {
+        await userService.getUniqueUser({
+          id: 0,
+        });
+      } catch (error) {
+        expect(error).toBeInstanceOf(UserNotFoundException);
+      }
+    });
+
+    it("should throw if email does not exist", async () => {
+      try {
+        await userService.getUniqueUser({
+          email: "janedoe@test.com",
+        });
+      } catch (error) {
+        expect(error).toBeInstanceOf(UserNotFoundException);
+      }
+    });
+
+    it("should throw if username does not exist", async () => {
+      try {
+        await userService.getUniqueUser({
+          userName: "jane",
+        });
+      } catch (error) {
+        expect(error).toBeInstanceOf(UserNotFoundException);
       }
     });
   });
