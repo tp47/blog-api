9b8c900ca79ff0395ee9c4953323fd2c8a0106fe
tp47 <adadu@tutanota.com>
2023-03-25 19:49:31 +0400

    Add /user/me route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from "@nestjs/common";
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Session,
+  UnauthorizedException,
+  UseInterceptors,
+} from "@nestjs/common";
+import { UserSession } from "src/auth/interfaces";
+import { UserEntity } from "src/utils/entities";
 
 @Controller("user")
-export class UserController {}
+export class UserController {
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get("me")
+  getMe(@Session() session: UserSession) {
+    if (!session.user) {
+      throw new UnauthorizedException("Not authenticated");
+    }
+    return new UserEntity(session.user);
+  }
+}
