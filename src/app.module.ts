1db8c30521bfd51dde000d4877e54d4b21667d90
tp47 <adadu@tutanota.com>
2023-04-03 21:41:52 +0400

    Add Posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from "./user/user.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from "./auth/auth.module";
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    PostModule,
   ],
 })
 export class AppModule {}
